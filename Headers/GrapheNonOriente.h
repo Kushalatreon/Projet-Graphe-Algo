70b73ba2860634a3ee7bb66ac3e0b2631fda4a70
Thibaud <Thibaud11>
2019-03-28 12:02:25 +0100

    Ecriture codage + décodage de Prüfer Test de ces fonctions dans le main

--- Headers/GrapheNonOriente.h
+++ Headers/GrapheNonOriente.h
@@ -14,7 +14,18 @@ public:
 
     virtual void adj_2_fs_aps(int* &fs, int* &aps) const override;
 
+    /**
+     * Créé un codage de Prüfer à partir d'une matrice d'adjacence
+     * @param [in] A - la matrice d'adjacence
+     * @param [out] C - le tableau qui accueillera le codage
+     */
+    void codagePrufer (int **A, int *&C );
 
+    /**
+     * Affiche le codage de Prüfer contenu dans C
+     * @param [in] C - tableau qui contient le codage
+     */
+    void decodagePrufer (int *C);
 
 private:
 };
--- Sources/GrapheNonOriente.cpp
+++ Sources/GrapheNonOriente.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "../Headers/GrapheNonOriente.h"
 
 GrapheNonOriente::GrapheNonOriente() : Graphe()
@@ -37,3 +38,100 @@ void GrapheNonOriente::adj_2_fs_aps(int* &fs, int* &aps) const
         fs[k++] = 0;
     }
 }
+
+void GrapheNonOriente::codagePrufer(int **A, int *&C) {
+    int n = A[0][0] - 2; //nombre de sommets - 2
+
+    C = new int [n + 1]; //nombre de sommets - 2, + 1 case pour le nombre de cases de C
+    C[0] = n;
+
+
+    for (int i = 1 ; i <= n + 2 ; ++i)
+        A[i][0] = 0;
+
+    for (int i = 1 ; i <= n + 2 ; ++i)
+        for (int j = 1 ; j <= n + 2 ; ++j)
+            if (A[i][j] == 1)
+            {
+                ++A[i][0];
+            }
+
+
+    for (int k = 1 ; k <= n ; ++k)
+    {
+        int i = 1, j = 1;
+
+        for (; A[i][0] != 1 ; ++i);
+        for (; A[i][j] != 1 ; ++j);
+
+        C[k] = j;
+        A[i][j] = 0;
+        A[j][i] = 0;
+        --A[i][0];
+        --A[j][0];
+    }
+}
+
+void GrapheNonOriente::decodagePrufer(int *C) {
+    //C[0] = l'indice du dernier élément
+    int n = C[0] + 2;
+    bool* I = new bool[n + 1];
+    int* nbOcc = new int[n + 1];
+    nbOcc[0] = n;
+
+    for (int i = 1 ; i <= n ; ++i)
+    {
+        I[i] = true;
+        nbOcc[i] = 0;
+    }
+
+    for (int i = 1 ; i < n - 1 ; ++i)
+        nbOcc[C[i]]++;
+
+    for (int i = 1 ; i < n - 1 ; ++i)
+    {
+        int s = C[i];
+        std::cout<<'('<<s<<',';
+
+        int t = 1;
+        while (!I[t] || nbOcc[t] > 0) {++t;}
+        std::cout<<t<<')'<<std::endl;
+        I[t] = false;
+        --nbOcc[s];
+    }
+
+    int s = 1;
+
+    for (; !I[s] ; ++s);
+    std::cout<<'('<<s<<',';
+    ++s;
+
+    for (; !I[s] ; ++s);
+    std::cout<<s<<')'<<std::endl;
+
+
+    delete[] I;
+    delete[] nbOcc;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
