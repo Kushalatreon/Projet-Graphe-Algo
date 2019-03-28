72c7790e4f832ce60092817277654e6a33921131
OwDemShad <galo.guccy@gmail.com>
2019-03-28 19:04:48 +0100

    Modification de Kruskal : "théoriquement" ok
    Modification de Kruskal pour que ça fonctionne.
    Plusieurs fonctions privées ont étés ajoutées : (je ferai la doc plus tard)
    - une struct arete : structure permettant de simuler une arete. Cela permet d'enlever un for de l'algo et que ce soit lisible lisible lisible
    - triAretes_ParBulle : tri par ordre croissant un tableau d'arêtes (Kruskal implique qu'on prenne les arrêtes dans l'ordre croissant)
    - getAretesTrieesParPoids : permet de justement remplir un tableau d'arêtes à partir du GrapheNonOriente courant
    - setAretes : prend un Graphe en paramètre ainsi qu'un tableau d'aretes. Permet de modifier la matrice d'adjacence de ce graphe pour renseigner uniquement les arêtes faisant partie de ce nouveau graphe (qui correspond à l'arbre recouvrant de poids minimal)

--- Headers/GrapheNonOriente.h
+++ Headers/GrapheNonOriente.h
@@ -14,7 +14,7 @@ public:
 
     virtual void adj_2_fs_aps(int* &fs, int* &aps) const override;
 
-    void Kruskal(GrapheNonOriente &h) const;
+    void Kruskal(GrapheNonOriente &h);
 
     /**
      * Créé un codage de Prüfer à partir d'une matrice d'adjacence
@@ -30,7 +30,25 @@ public:
     void decodagePrufer (int *C);
 
 private:
+
+    /*
+     * Le principe de l'algorithme consiste a creer un tableau d'arete (struct) qui contiendra les differentes aretes du graphe courant
+     * Ensuite, uniquement certaines aretes seront retenues pour faire parie de l'abre recouvrant de poids minimal
+     */
+    // represente une arete
+    typedef struct
+    {
+        int s,t;
+        int p;
+    } arete;
+
     void fusion(int i, int j, int* prem, int* &pilch, int* &cfc) const;
+
+    void triAretes_ParBulle(arete *&_a, int taille) const;
+
+    void getAretesTrieesParPoids(arete *&_a, int taille) const;
+
+    void setAretes(GrapheNonOriente &h, const arete *_a, int taille);
 };
 
 
--- Sources/GrapheNonOriente.cpp
+++ Sources/GrapheNonOriente.cpp
@@ -38,6 +38,61 @@ void GrapheNonOriente::adj_2_fs_aps(int* &fs, int* &aps) const
     }
 }
 
+
+void GrapheNonOriente::triAretes_ParBulle(GrapheNonOriente::arete *&_a, int taille) const
+{
+    bool estTrie = false;
+    while(!estTrie)
+    {
+        estTrie = true;
+        for(int i = 0 ; i < taille-1 ; i++)
+        {
+            if(_a[i].p > _a[i+1].p)
+            {
+                std::swap(_a[i], _a[i+1]);
+                estTrie = false;
+            }
+        }
+
+        --taille;
+    }
+}
+
+void GrapheNonOriente::getAretesTrieesParPoids(GrapheNonOriente::arete *&_a, int taille) const
+{
+    // remplissage du tableau d'aretes
+    for(int i = 1 ; i <= d_adj[0][0] ; ++i)
+    {
+        for (int j = 1; j <= d_adj[0][0] ; ++j)
+        {
+            if(d_adj[i][j] != 0)
+            {
+                _a[i].t = i;
+                _a[i].s = j;
+                _a[i].p = d_adj[i][j];
+            }
+        }
+    }
+
+    // tri du tableau d'aretes
+    triAretes_ParBulle(_a, taille);
+}
+
+void GrapheNonOriente::setAretes(GrapheNonOriente &h, const GrapheNonOriente::arete *_a, int taille)
+{
+    for ( int i = 0 ; i < taille ; ++i)
+    {
+        int s = _a[i].s;
+        int t = _a[i].t;
+        int p = _a[i].p;
+
+        if(_a[i].p != 0)
+        {
+            h.d_adj[t][s] = p;
+        }
+    }
+}
+
 void GrapheNonOriente::fusion(int i, int j, int *prem, int *&pilch, int *&cfc) const
 {
     // i et j sont des numeros de composantes
@@ -56,20 +111,49 @@ void GrapheNonOriente::fusion(int i, int j, int *prem, int *&pilch, int *&cfc) c
     }
 }
 
-void GrapheNonOriente::Kruskal(GrapheNonOriente &h) const
+void GrapheNonOriente::Kruskal(GrapheNonOriente &h)
 {
+    // nombre de sommets
+    int n = d_adj[0][0];
+
+    // nombre d'aretes
+    int m = d_adj[0][1];
+
     // Appel au constructeur par recopie pour creer l'arbe h avec le nombre de sommets du graphe courant
-    h = {d_adj[0][0]};
+    h = {n};
 
     // Le nombre d'aretes, dans un arbre de Kruskal, est egal au nombre de sommets - 1
     h.d_adj[0][1] = h.d_adj[0][0] - 1;  // <=> = d_adj[0][0]
 
-    int n = d_adj[0][0];
+    // d_adj[0][1] = nb aretes du graphe courant
+    // aretes de l'arbre recouvrant minimal
+    arete* aretesARM = new arete[m];      // <=> h.a dans l'algorithme du cours
+
+    // aretes du graphe courant classees selon leur poids par ordre croissant
+    // aretes du graphe courant triees par ordre croissant selon leur poids
+    arete* aretesGCT = new arete[m];      // <=> g.a dans l'algorithme du cours
+
+    // remplissage du tableau d'arete du  graphe courant
+    getAretesTrieesParPoids ( aretesGCT, m ) ;
+
+
+    /*
+     * A partir de la, aretesGCT represente les aretes du graphe courant (celui sur lequel on applique l'algorithme de Kruskal).
+     * Les aretes sont triees par ordre croissant de leur poids et possedent :
+     * - un predecesseur (t)
+     * - un successeur   (s)
+     * - un poids        (p)
+     */
+
+
+
+
     int *prem = new int[n + 1];
     int *pilch = new int[n + 1];
     int *cfc = new int[n + 1];
 
-    for ( int i = 1 ; i<= n ; ++i)
+    // initialisation de prem, pilch et cfc
+    for ( int i = 1 ; i <= n ; ++i)
     {
         prem[i] = i;
         pilch[i] = 0;
@@ -78,21 +162,19 @@ void GrapheNonOriente::Kruskal(GrapheNonOriente &h) const
 
     for ( int i = 1, k = 0 ; i <= n && k < n ; ++i)
     {
-        int *tmp = new int[n + 1];
-        for ( int j = 1 ; j <= n ; j++) tmp[j] = d_adj[i][j];
+        arete e = aretesGCT[i];
 
-        for ( int j = 1 ; j <= n ; ++j)
+        if(cfc[e.s] != cfc[e.t])
         {
-            if(cfc[i] != cfc[j])
-            {
-                h.d_adj[i][k++] = tmp[j];
-                fusion ( cfc[i], cfc[j], prem, pilch, cfc);
-            }
+            aretesARM[k++] = e;
+            fusion ( cfc[e.s], cfc[e.t], prem, pilch, cfc);
         }
-
-        delete[] tmp;
     }
 
+    setAretes(h, aretesARM, m);
+
+    delete[] aretesARM;
+    delete[] aretesGCT;
     delete[] prem;
     delete[] pilch;
     delete[] cfc;
@@ -172,3 +254,9 @@ void GrapheNonOriente::decodagePrufer(int *C) {
     delete[] I;
     delete[] nbOcc;
 }
+
+
+
+
+
+
