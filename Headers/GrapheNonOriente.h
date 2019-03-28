6758942ebc7da51c8f11d6e380c3b0c2d9f63151
Thibaud11 <45309550+Thibaud11@users.noreply.github.com>
2019-03-28 13:06:31 +0100

    MAJ pour ajout Prüfer

--- Headers/GrapheNonOriente.h
+++ Headers/GrapheNonOriente.h
@@ -16,6 +16,18 @@ public:
 
     void Kruskal(GrapheNonOriente &h) const;
 
+    /**
+     * Créé un codage de Prüfer à partir d'une matrice d'adjacence
+     * @param [in] A - la matrice d'adjacence
+     * @param [out] C - le tableau qui accueillera le codage
+     */
+    void codagePrufer (int **A, int *&C );
+
+    /**
+     * Affiche le codage de Prüfer contenu dans C
+     * @param [in] C - tableau qui contient le codage
+     */
+    void decodagePrufer (int *C);
 
 private:
     void fusion(int i, int j, int* prem, int* &pilch, int* &cfc) const;
